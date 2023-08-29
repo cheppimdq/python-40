1b77bb4c1f97d035d238ccd66e4efec2e9c9d9c1
cheppimdq <cheppimdp@gmail.com>
2023-08-29 20:27:08 -0300

    ejercicios

---
+++ Dividir.py
@@ -0,0 +1,6 @@
+nro1 = int(input("ingrese dividendo"))
+nro2 = int(input("ingrese divisor"))
+if nro2 == 0:
+    print("no se puede dividir por 0")
+else:
+    print(nro1/nro2)
---
+++ Edad.py
@@ -0,0 +1,5 @@
+age = int(input("ingrese su edad"))
+if age < 18 :
+    print("menor de edad")
+else:
+    print("mayor de edad")
