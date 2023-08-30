59170e8f9ee480f7869fee8db21b8ccdbed0983f
cheppimdq <cheppimdp@gmail.com>
2023-08-29 22:10:08 -0300

    Modulo 2

---
+++ Modulo2/VariableBoleanas.py
@@ -0,0 +1,20 @@
+# variables
+
+print(4 < 5) # ---> True
+
+print(4 > 5) # ---> False
+
+# comparaciones
+
+print(6 == 6) #doble == comparaciones / 1 solo = asignaciones --> True
+
+print (6 == 4) # --> false
+
+x = True
+y = False
+
+print(type(x))
+print(type(y))
+
+print(x)
+print(y)
